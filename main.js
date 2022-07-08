41e230989de89094f70acdb1081913838b329421
Link <95591923+LinkGXMasterPlayer2@users.noreply.github.com>
2022-07-07 17:41:08 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,61 @@
+noseX = 0;
+noseY = 0;
+
+function preload(){
+ 
+clownNose = loadImage('https://i.postimg.cc/bYLncb0b/R.png');
+
+}
+
+
+function setup(){
+  
+    canvas = createCanvas(500,500);
+    canvas.center();
+    video = createCapture(VIDEO);
+    video.size(500,500);
+    video.hide();
+
+    poseNet = ml5.poseNet(video,modelLoaded);
+    poseNet.on('pose',gotPoses);
+    
+    
+
+}
+
+
+function modelLoaded(){
+
+    console.log("PoseNet is initialized");
+
+}
+
+function gotPoses(results){
+
+    if(results.length > 0){
+
+        console.log(results);
+        noseX = results[0].pose.nose.x - 30;
+        noseY = results[0].pose.nose.y - 5;
+        console.log("nose x = " + noseX);
+        console.log("nose y = " + noseY);
+
+
+}
+
+
+}
+
+function draw(){
+
+image(video,0,0,500,500);
+image(clownNose,noseX,noseY,60,60);
+
+
+ 
+}
+
+function takeSnapshot(){
+
+    save("MyClownNoseFilterImage.png");
+}
